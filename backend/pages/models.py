8bbeccf80ada0d97f46d5ffc1f32a790d6d5f9ac
nam <nam@njit.edu>
2026-05-10 12:00:13 -0400

    removed HEAD and other markings

--- backend/pages/models.py
+++ backend/pages/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 
-<<<<<<< HEAD
 class Profile(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='supplement_images/')
@@ -14,7 +13,6 @@ class MyModel(models.Model):
 
     def __str__(self):
         return self.name
-=======
 
 # Rating system for products (vitamins)
 class Rating(models.Model):
@@ -35,4 +33,3 @@ class Rating(models.Model):
 
     def __str__(self):
         return f"{self.score} stars"
->>>>>>> 7441771dde0d08e4e136dc00d05ba3eea2b09dc5
